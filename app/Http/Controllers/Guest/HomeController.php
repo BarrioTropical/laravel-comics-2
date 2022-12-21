24378254617e4c85f2d0c357de62e58343dd5a25
“BarrioTropical” <barrio>
2022-12-21 15:38:00 +0100

    layout + img
    Signed-off-by: “BarrioTropical” <“barrio>

--- app/Http/Controllers/Guest/HomeController.php
+++ app/Http/Controllers/Guest/HomeController.php
@@ -4,8 +4,11 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    //
+    public function index(){
+        return view('comics.index');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Guest\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/comics', [HomeController::class, 'index'])->name('comics.index');
 
